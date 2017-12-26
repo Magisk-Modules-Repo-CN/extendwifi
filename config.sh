51e5303b3974edf8ded12baf937bceabe1ff0c05
Thomas van Tilburg <thomasvt@me.com>
2017-12-26 20:26:10 +0100

    Fix install issues

--- config.sh
+++ config.sh
@@ -40,9 +40,10 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
-  ui_print "*******************************"
+  ui_print "***********************************"
+  ui_print "   Extend your WiFi capability's"
+  ui_print "            by Thomasvt"
+  ui_print "***********************************"
 }
 
 ##########################################################################################
